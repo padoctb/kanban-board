b0b145c68e53e4cc8417c16da867f9dddac11faa
padoctb <yeahdima@gmail.com>
2019-08-15 13:03:20 +0300

    Updates

---
+++ precache-manifest.46ac55df73af166beb5d788f57262f9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5611ad1dabeba4159ae9b37edc9b9add",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "9e6245c4e534a1831628",
+    "url": "/kanban-board/static/css/main.4cd554a5.chunk.css"
+  },
+  {
+    "revision": "3683647b9d6dfbbae098",
+    "url": "/kanban-board/static/js/2.ae092f32.chunk.js"
+  },
+  {
+    "revision": "9e6245c4e534a1831628",
+    "url": "/kanban-board/static/js/main.6b7f4f6b.chunk.js"
+  },
+  {
+    "revision": "af90c4ec1ffd2642c806",
+    "url": "/kanban-board/static/js/runtime~main.2a900544.js"
+  },
+  {
+    "revision": "57cae6c1d6a451c46927e2b56ac3073a",
+    "url": "/kanban-board/static/media/cancel.57cae6c1.svg"
+  },
+  {
+    "revision": "03b9bf5fb18c8b89f92049b2fda23aa9",
+    "url": "/kanban-board/static/media/edit.03b9bf5f.svg"
+  },
+  {
+    "revision": "2393e40b59d42acb9bfce2c54d89c091",
+    "url": "/kanban-board/static/media/remove.2393e40b.svg"
+  }
+]);
